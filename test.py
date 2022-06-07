535efd6400cb4eb710d838911251f4903fdbb246
adrianuscharlie <adrianus.charlie02@gmail.com>
2022-06-07 12:51:17 +0700

    Editing Predict Script to Enable Predict Via Server and Using Image URL

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-import sys
-print("cmd entry",sys.argv[1])
+import skimage
+import numpy as np
+
+image_filename = "https://upload.wikimedia.org/wikipedia/commons/f/f0/Acne_vulgaris_on_a_very_oily_skin.jpg"
+image_numpy = skimage.io.imread( image_filename )
+image_numpy=np.asarray(image_numpy).astype(np.float32)/255
+print(image_numpy)
